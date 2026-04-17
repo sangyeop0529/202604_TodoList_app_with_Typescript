ceca72ff0309b121ee86150c36ce478e17a9d6ed
sangyeop0529 <sangyeop0529@gmail.com>
2026-04-17 10:26:53 +0900

    완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useRef, useState } from "react";
+import { useReducer, useRef } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 import "./App.css";
 import type { Todo } from "./type";
 
+type Action =
+  | { type: "CREATE"; data: Todo }
+  | { type: "UPDATE"; targetId: number }
+  | { type: "DELETE"; targetId: number };
+
+function reducer(state: Todo[], action: Action): Todo[] {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item,
+      );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
   const idRef = useRef<number>(3);
-
   const onCreate = (content: string): void => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-
-    setTodos([...todos, newTodo]);
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   };
 
   const onUpdate = (targetId: number): void => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo,
-      ),
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   };
 
   const onDelete = (targetId: number): void => {
-    setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
   };
 
   return (
